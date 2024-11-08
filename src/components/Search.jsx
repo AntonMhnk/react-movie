900e5665b238eff8cec3f2c09693893e32786452
AntonMhnk <anton.mhnk@yandex.ru>
2024-11-08 19:13:44 +0500

    edit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,74 +1,73 @@
-import React from "react"
+import React, {useState} from "react"
 
-class Search extends React.Component {
-    state = {
-        seacrh: '',
-		type: 'all'
-    }
+function Search(props) {
+	const {seacrhMovies = Function.prototype} = props;
+
+
+	const [seacrh, setSearch] = useState("");
+	const [type, setType] = useState("all");
     
-	handelKey = (event) => {
+	const handelKey = (event) => {
 		if (event.key === "Enter") {
-			this.props.seacrhMovies(this.state.seacrh, this.state.type)
+			seacrhMovies(seacrh, type)
 		}
 	}
 
-	handelFiler = (event) => {
-		this.setState(() => ({type: event.target.dataset.type}), () => {
-			this.props.seacrhMovies(this.state.seacrh, this.state.type)
-		})
+	const handelFiler = (event) => {
+		setType(event.target.dataset.type);
+		seacrhMovies(seacrh, event.target.dataset.type)
 	}
 
-    render() {
         return  <div className="row">
           <div className="input-field">
             <input id="email_inline" 
 					type="search" 
 					className="validate"
 					placeholder="Search"
-					value={this.state.seacrh}
-					onChange={(e) => this.setState({seacrh: e.target.value})}
-					onKeyDown={this.handelKey}
+					value={seacrh}
+					onChange={(e) => setSearch(e.target.value)}
+					onKeyDown={handelKey}
 					/>
-					<button className="waves-effect waves-light btn" 
-							onClick={() => this.props.seacrhMovies(this.state.seacrh, this.state.type)}>
-							Search
-					</button>
+			<button className="waves-effect waves-light btn" 
+					onClick={() => seacrhMovies(seacrh, type)}>
+					Search
+			</button>
           </div>
 		  
 		  <div>
-				<label>
-					<input className="with-gap" 
-							name="type" 
-							type="radio" 
-							data-type="all" 
-							onChange={this.handelFiler} 
-							checked={this.state.type === 'all'}
-							/>
-					<span>All</span>
-				</label>
-				<label>
-					<input className="with-gap" 
-							name="type" 
-							type="radio" 
-							data-type="movie" 
-							onChange={this.handelFiler} 
-							checked={this.state.type === 'movie'}
-							/>
-					<span>Movies only</span>
-				</label>
-				<label>
-					<input className="with-gap"  
-							name="type"  
-							type="radio"  
-							data-type="series" 
-							onChange={this.handelFiler} 
-							checked={this.state.type === 'series'}
-							/>
-					<span>Series only</span>
-				</label>
+			<label>
+				<input className="with-gap" 
+						name="type" 
+						type="radio" 
+						data-type="all" 
+						onChange={handelFiler} 
+						checked={type === 'all'}
+						/>
+				<span>All</span>
+			</label>
+			<label>
+				<input className="with-gap" 
+						name="type" 
+						type="radio" 
+						data-type="movie" 
+						onChange={handelFiler} 
+						checked={type === 'movie'}
+						/>
+				<span>Movies only</span>
+			</label>
+			<label>
+				<input className="with-gap"  
+						name="type"  
+						type="radio"  
+						data-type="series" 
+						onChange={handelFiler} 
+						checked={type === 'series'}
+						/>
+				<span>Series only</span>
+			</label>
 		  </div>
         </div>
     }
-}
+
 
 export {Search}
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
     return <nav className="blue darken-4">
     <div className="nav-wrapper">
-      <a href="!#" className="brand-logo">React Movies</a>
+      <a href="https://antonmhnk.github.io/react-movie/" className="brand-logo">React Movies</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="!#">Repo</a></li>
+        <li><a href="https://github.com/AntonMhnk/react-movie/tree/main" target="_blank" rel="noreferrer">Repo</a></li>
       </ul>
     </div>
   </nav>
